e7888bc3d556418471fa765b5d78145c9bca52c4
XTusers <40054290+XTusers@users.noreply.github.com>
2018-07-30 07:04:10 +0530

    my web201 assignment

---
+++ 122463_Danish/webpack.config.js
@@ -0,0 +1,79 @@
+// webpack.config.js
+var path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = {
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "min.css",
+    }),
+    new CopyWebpackPlugin([{
+        from: 'index.html',
+        to: './index.html'
+      },
+      {
+        from: 'checkout.html',
+        to: './checkout.html'
+      },
+      {
+        from: 'cart.html',
+        to: './cart.html'
+      }, {
+        from: 'src/images',
+        to: 'images'
+      }
+
+    ]),
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    }),
+
+
+  ],
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["es2015"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  },
+  node: {
+    fs: 'empty',
+    // tls: 'empty'
+  }
+};
